367582910984e486d8160471d3a8b8a5635bbf79
sivasb1019 <113979953+sivasb1019@users.noreply.github.com>
2023-10-21 00:53:03 +0530

    creds.py

--- creds.py
+++ creds.py
@@ -1,1 +1,2 @@
-API_Key="the_API_KEY_is_secures_key_so_I_changed_the_key_before_submiting_application"
+#Add the correct API key from you ILovePdf API developer account
+API_Key="the_API_KEY_is_secured_key_so_I_changed_the_key_before_submiting_application"
